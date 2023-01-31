254f9ed86b0524f68898ef7e2e22327ac0a901c9
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2023-01-31 22:53:54 +0200

    little bit houskeeping. Deleted unneeded code

--- src/components/UserInterface.js
+++ src/components/UserInterface.js
@@ -1,22 +1,20 @@
 const UserInterface = ({title,showElement,text,ShowWord,Keyboard,button}) => {
     
     return (
-        <div className="">
-            <div className="game row">
-                <div className="infobox col-2">                
-                        Try to guess the word!        
-                </div>
-                <div className="GameBox col-8">
-                    <div className="titleBox row">
-                        <h1>{title}</h1>
-                    </div>                    
-                    <div className="justify-content-center imageBox row">
-                        {showElement}
-                        {text}
-                        {ShowWord}
-                        {Keyboard}
-                        {button}
-                    </div>
+        <div className="row">
+            <div className="infobox col-2">                
+                    Try to guess the word!        
+            </div>
+            <div className="GameBox col-8">
+                <div className="titleBox row">
+                    <h1>{title}</h1>
+                </div>                    
+                <div className="imageBox row">
+                    {showElement}
+                    {text}
+                    {ShowWord}
+                    {Keyboard}
+                    {button}
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import 'bootstrap/dist/js/bootstrap.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='pt-2'>
       <App />
-    </div>
   </React.StrictMode>,
 );
