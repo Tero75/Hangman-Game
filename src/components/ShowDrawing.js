b9c27915ad7ec7b2754591fdf7064f4b5cf36ee0
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2023-03-15 22:10:09 +0200

    img tag now has alt prop

--- src/components/ShowDrawing.js
+++ src/components/ShowDrawing.js
@@ -2,7 +2,7 @@ const ShowDrawing = ({HangmanImages,drawingIndex}) => {
 
     return (
         <div>
-            <img src={HangmanImages[drawingIndex]}/>
+            <img src={HangmanImages[drawingIndex]} alt=''/>
         </div>
     )
 }
