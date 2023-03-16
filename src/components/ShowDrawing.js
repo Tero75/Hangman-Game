369cfbf693f81079f1960c8ebba8e73f018e6311
Tero Leinonen <66237903+Tero75@users.noreply.github.com>
2023-03-16 23:06:13 +0200

    now with img alt prop with telling strings

--- src/components/ShowDrawing.js
+++ src/components/ShowDrawing.js
@@ -1,8 +1,8 @@
 const ShowDrawing = ({HangmanImages,drawingIndex}) => {
-
+    //console.log(HangmanImages[drawingIndex].ALT)
     return (
         <div>
-            <img src={HangmanImages[drawingIndex]} alt=''/>
+            <img src={HangmanImages[drawingIndex].SRC} alt={HangmanImages[drawingIndex].ALT}/>
         </div>
     )
 }
